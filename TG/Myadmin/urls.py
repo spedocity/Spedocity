3be5c314ade3f4b993152146e4dc427c115a08d3
akhileshhs <hs.akhileshshivu@gmail.com>
2025-01-10 10:20:12 +0530

    admin

--- TG/Myadmin/urls.py
+++ TG/Myadmin/urls.py
@@ -4,5 +4,6 @@ from Myadmin import views
 urlpatterns = [
     path('', views.ahome, name='ahome'),
     path('Orders',views.aorder,name='aorder'),
-    path('Employee',views.aemployee,name='aemployee')
+    path('Employee',views.aemployee,name='aemployee'),
+    path('customers/', views.customer_list_view, name='customer_list'),
 ]
--- TG/Myadmin/views.py
+++ TG/Myadmin/views.py
@@ -1,12 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import date
+from customer.models import Order, Customer
+from vparnter.models import PartnerInfo
 
 # Create your views here.
 def ahome(request):
-    return render(request,"Ahome.html")
+    # Calculate statistics
+    total_partners = PartnerInfo.objects.count()  # Total number of partners
+    total_customers = Customer.objects.count()  # Total number of customers
+    orders_today = Order.objects.filter(date=date.today()).count()  # Orders placed today
+    total_orders = Order.objects.count()  # Total number of orders
+
+    context = {
+        'total_partners': total_partners,
+        'total_customers': total_customers,
+        'orders_today': orders_today,
+        'total_orders': total_orders,
+    }
+    return render(request,"Ahome.html",context)
 
 def aorder(request):
-    return render(request,"a_orders.html")
+    orders = Order.objects.all().order_by('-date')
+
+    context = {
+        'orders': orders,
+    }
+    return render(request,"a_orders.html",context)
 
 def aemployee(request):
     return render(request,'aemployee.html')
+
+def customer_list_view(request):
+    # Fetch all customers from the database
+    customers = Customer.objects.all()
+
+    context = {
+        'customers': customers,
+    }
+    return render(request, 'customer_list.html', context)
